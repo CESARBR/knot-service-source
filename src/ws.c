ac1d3965391792b10c89862edc2e1671ffdb4f81
Claudio Koiti Takahasi <ckt@cesar.org.br>
2015-07-08 17:05:16 -0300

    gw: Add websockets protocol driver
    This patch adds the initial files for websockets protocol driver. User
    must run knotd --proto=ws|http to select the wanted Meshblu IoT
    protocol. ws (websockets) is the default protocol when the option
    'proto' is not informed.

--- src/ws.c
+++ src/ws.c
@@ -27,82 +27,38 @@
  */
 
 #include <stdio.h>
-#include <errno.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
+#include <stdint.h>
 
-#include <glib.h>
-
-#include "manager.h"
+#include "proto.h"
 #include "ws.h"
-#include "http.h"
 
-static GMainLoop *main_loop;
-
-/* Default is websockets */
-static const char *opt_proto = "ws";
-
-static void sig_term(int sig)
+static int ws_signup(void)
 {
-	g_main_loop_quit(main_loop);
+	return 0;
 }
 
-static GOptionEntry options[] = {
-	{ "proto", 'p', 0, G_OPTION_ARG_STRING, &opt_proto,
-					"protocol", "eg: http or ws" },
-	{ NULL },
+static int ws_signin(const char *token)
+{
+	return 0;
+}
+
+static void ws_signoff(int sock)
+{
+
+}
+
+static struct proto_ops ops = {
+	.signup = ws_signup,
+	.signin = ws_signin,
+	.signoff= ws_signoff,
 };
 
-int main(int argc, char *argv[])
+int ws_register(void)
 {
-	GOptionContext *context;
-	GError *gerr = NULL;
-	int err;
+	return proto_ops_register(&ops);
+}
 
-	printf("KNOT Gateway\n");
-
-	context = g_option_context_new(NULL);
-	g_option_context_add_main_entries(context, options, NULL);
-
-	if (!g_option_context_parse(context, &argc, &argv, &gerr)) {
-		printf("Invalid arguments: %s\n", gerr->message);
-		g_error_free(gerr);
-		exit(EXIT_FAILURE);
-	}
-
-	signal(SIGTERM, sig_term);
-	signal(SIGINT, sig_term);
-
-	main_loop = g_main_loop_new(NULL, FALSE);
-
-	/* FIXME: Use a better mechanism to select dynamically protos */
-	if (strcmp(opt_proto, "http") == 0)
-		http_register();
-	else
-		ws_register();
-
-	err = manager_start();
-	if (err < 0) {
-		printf("start(): %s (%d)\n", strerror(-err), -err);
-		goto done;
-	}
-
-	g_main_loop_run(main_loop);
-
-	g_main_loop_unref(main_loop);
-
-	if (strcmp(opt_proto, "http") == 0)
-		http_unregister();
-	else
-		ws_unregister();
-
-	manager_stop();
-
-	printf("Exiting\n");
-
-	return err;
-
-done:
-	return err;
+void ws_unregister(void)
+{
+	proto_ops_unregister(&ops);
 }
