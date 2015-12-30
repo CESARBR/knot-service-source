071994b6e2fe40aef2683166d9ca3205ef16a818
Claudio Takahasi <ckt@cesar.org.br>
2015-12-30 15:34:00 -0300

    unit: Add testing register W/O device name
    This patch adds a unit test to verify if the gateway handles properly
    a malformed registration message. This unit test verifies if the
    gateway returns a proper error when 'register' message doesn't include
    the device name.

--- unit/ktest.c
+++ unit/ktest.c
@@ -27,6 +27,7 @@
  */
 
 #include <stdio.h>
+#include <stdint.h>
 #include <errno.h>
 #include <unistd.h>
 #include <sys/socket.h>
@@ -34,10 +35,16 @@
 
 #include <glib.h>
 
+#include <proto-net/knot_proto_net.h>
+#include <proto-app/knot_proto_app.h>
+
 #include "src/log.h"
 
 /* Abstract unit socket namespace */
 #define KNOT_UNIX_SOCKET			"knot"
+
+/* device name for the register */
+#define	KTEST_DEVICE_NAME			"ktest_unit_test"
 
 static int sockfd;
 
@@ -80,13 +87,60 @@ static void close_test(void)
 	sockfd = -1;
 }
 
+static ssize_t do_request(const knot_msg *kmsg, size_t len, knot_msg *kresp)
+{
+	ssize_t nbytes;
+	int err;
+
+	nbytes = write(sockfd, kmsg, len);
+	if (nbytes < 0) {
+		err = errno;
+		LOG_ERROR("write(): %s(%d)\n", strerror(err), err);
+		return -err;
+	}
+
+	nbytes = read(sockfd, kresp, sizeof(knot_msg));
+	if (nbytes < 0) {
+		err = errno;
+		LOG_ERROR("read(): %s (%d)\n", strerror(err), err);
+		return -err;
+	}
+
+	return nbytes;
+}
+
+static void register_test_missing_devname(void)
+{
+	knot_msg kmsg, kresp;
+	ssize_t size;
+
+	memset(&kmsg, 0, sizeof(knot_msg));
+	kmsg.hdr.type = KNOT_MSG_REGISTER_REQ;
+
+	/* Sending register message with missing parameters. */
+
+	kmsg.hdr.payload_len = 0;
+	size = do_request(&kmsg, sizeof(kmsg.reg), &kresp);
+
+	/* Response consistency */
+	g_assert(size == sizeof(kresp.action));
+	g_assert(kresp.hdr.payload_len == sizeof(kresp.action.result));
+
+	/* Response opcode & result */
+	g_assert(kresp.hdr.type == KNOT_MSG_REGISTER_RESP);
+	g_assert(kresp.action.result == KNOT_INVALID_DATA);
+}
+
 /* Register and run all tests */
 int main(int argc, char *argv[])
 {
 	g_test_init (&argc, &argv, NULL);
 
-	/* TEST 1: Unix socket connection  */
 	g_test_add_func("/1/connect", connection_test);
+
+	g_test_add_func("/1/register_missing_devname",
+				register_test_missing_devname);
+
 	g_test_add_func("/1/close", close_test);
 
 	return g_test_run();
