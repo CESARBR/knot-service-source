23f9d121a474b29355c4e0d9b03467dded8b0c3d
Claudio Takahasi <ckt@cesar.org.br>
2015-12-30 15:38:46 -0300

    unit: Add register with invalid payload length
    This patch adds a unit test to verify if the gateway handles properly
    a malformed registration message. This unit test verifies if the gateway
    catches payload with invalid length.

--- unit/ktest.c
+++ unit/ktest.c
@@ -131,6 +131,27 @@ static void register_test_missing_devname(void)
 	g_assert(kresp.action.result == KNOT_INVALID_DATA);
 }
 
+static void register_test_invalid_payload_len(void)
+{
+	knot_msg kmsg, kresp;
+	ssize_t size;
+
+	memset(&kmsg, 0, sizeof(knot_msg));
+	kmsg.hdr.type = KNOT_MSG_REGISTER_REQ;
+
+	/* One additional octet: larger than expected msg length  */
+	kmsg.hdr.payload_len = sizeof(kmsg.reg) - sizeof(kmsg.hdr) + 1;
+	size = do_request(&kmsg, sizeof(kmsg.reg), &kresp);
+
+	/* Response consistency */
+	g_assert(size == sizeof(kresp.action));
+	g_assert(kresp.hdr.payload_len == sizeof(kresp.action.result));
+
+	/* Response opcode & result */
+	g_assert(kresp.hdr.type == KNOT_MSG_REGISTER_RESP);
+	g_assert(kresp.action.result == KNOT_INVALID_DATA);
+}
+
 /* Register and run all tests */
 int main(int argc, char *argv[])
 {
@@ -140,6 +161,8 @@ int main(int argc, char *argv[])
 
 	g_test_add_func("/1/register_missing_devname",
 				register_test_missing_devname);
+	g_test_add_func("/1/register_invalid_payload_len",
+				register_test_invalid_payload_len);
 
 	g_test_add_func("/1/close", close_test);
 
