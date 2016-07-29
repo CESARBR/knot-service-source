5a095bc80ed76e7ac7a2577fa078ef90a9425a55
Claudio Takahasi <ckt@cesar.org.br>
2016-07-29 14:31:15 -0300

    core: Add host and port for SPI passthrough mode
    This patch adds two new options to allow nrfd works with a remote
    nRF24L01 radio expose through TCP socket.

--- src/nrfd.c
+++ src/nrfd.c
@@ -27,9 +27,57 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <glib.h>
+
+#include "log.h"
+
+static GMainLoop *main_loop;
+
+static const char *opt_host = NULL;
+static unsigned int opt_port = 9000;
+
+static void sig_term(int sig)
+{
+	g_main_loop_quit(main_loop);
+}
+
+static GOptionEntry options[] = {
+	{ "host", 'h', 0, G_OPTION_ARG_STRING, &opt_host,
+					"host", "Host exposing nRF24L01 SPI" },
+	{ "port", 'p', 0, G_OPTION_ARG_INT, &opt_port,
+					"port", "Remote port" },
+	{ NULL },
+};
 
 int main(int argc, char *argv[])
 {
-	printf("KNOT nrfd\n");
+	GOptionContext *context;
+	GError *gerr = NULL;
+
+	LOG_INFO("KNOT nrfd\n");
+
+	context = g_option_context_new(NULL);
+	g_option_context_add_main_entries(context, options, NULL);
+
+	if (!g_option_context_parse(context, &argc, &argv, &gerr)) {
+		LOG_ERROR("Invalid arguments: %s\n", gerr->message);
+		g_error_free(gerr);
+		g_option_context_free(context);
+		return EXIT_FAILURE;
+	}
+
+	g_option_context_free(context);
+
+	signal(SIGTERM, sig_term);
+	signal(SIGINT, sig_term);
+	signal(SIGPIPE, SIG_IGN);
+
+	main_loop = g_main_loop_new(NULL, FALSE);
+
+	g_main_loop_run(main_loop);
+
+	g_main_loop_unref(main_loop);
+
 	return 0;
 }
