108b5ae4c8f0c18139ad84cc6d50ebce425a88c0
Claudio Takahasi <ckt@cesar.org.br>
2017-12-20 15:55:59 -0300

    inetbr: Add unix socket connection
    This patch adds the initial support to route incoming IPv4/IPv6
    datagram traffic to knotd. Each client(peer) will have a unix socket
    between inetbrd and knotd in order manage authenticated sessions
    and handle asynchronous traffic from knotd/cloud.

--- inetbr/manager.c
+++ inetbr/manager.c
@@ -38,6 +38,7 @@
 
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <sys/un.h>
 #include <netinet/in.h>
 
 #include <glib.h>
@@ -46,31 +47,133 @@
 
 #include "manager.h"
 
+/* Abstract unit socket namespace */
+#define KNOT_UNIX_SOCKET			"knot"
+
 static gint io6_watch = -1;
 static gint io4_watch = -1;
+static GHashTable *inet_hash = NULL;
 
-static gboolean read_inet4_cb(GIOChannel *io, GIOCondition cond,
+struct watch {
+	int id;
+	int sock;
+};
+
+static int unix_connect(void)
+{
+	struct sockaddr_un addr;
+	int err, sock;
+
+	sock = socket(AF_UNIX, SOCK_SEQPACKET | SOCK_CLOEXEC, 0);
+	if (sock < 0) {
+		err = errno;
+		hal_log_error("unix socket(): %s (%d)", strerror(err), err);
+		return -err;
+	}
+
+	memset(&addr, 0, sizeof(addr));
+	addr.sun_family = AF_UNIX;
+	/* Abstract namespace: first character must be null */
+	strcpy(addr.sun_path + 1, KNOT_UNIX_SOCKET);
+
+	if (connect(sock, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
+		err = errno;
+		hal_log_error("unix connect(): %s (%d)", strerror(err), err);
+		close(sock);
+		return -err;
+	}
+
+	return sock;
+}
+
+static void downlink_destroy(gpointer user_data)
+{
+	g_hash_table_remove(inet_hash, user_data);
+
+	g_free(user_data);
+}
+
+static gboolean downlink_cb(GIOChannel *io, GIOCondition cond,
 							gpointer user_data)
 {
-	struct sockaddr_in addr4;
 	char buffer[1280];
-	char ipv4_str[INET_ADDRSTRLEN];
-	socklen_t addrlen;
 	ssize_t len;
-	int sock;
+	int sock, err;
 
 	if (cond & (G_IO_ERR | G_IO_HUP | G_IO_NVAL))
 		return FALSE;
 
 	sock = g_io_channel_unix_get_fd(io);
+	len = read(sock, buffer, sizeof(buffer));
+	if (len < 0) {
+		err = errno;
+		hal_log_error("read(): %s(%d)", strerror(err), err);
+		return TRUE;
+	}
+
+	/* TODO: Route do peer */
+
+	return TRUE;
+}
+
+static gboolean read_inet4_cb(GIOChannel *io, GIOCondition cond,
+							gpointer user_data)
+{
+	GIOCondition down_cond = G_IO_ERR | G_IO_HUP | G_IO_NVAL | G_IO_IN;
+	struct sockaddr_in addr4;
+	struct watch *watch;
+	char buffer[1280];
+	char ipv4_str[INET_ADDRSTRLEN];
+	socklen_t addrlen;
+	ssize_t len;
+	int err;
+	int cli_sock;
+	int sock;
+
+	if (cond & (G_IO_ERR | G_IO_HUP | G_IO_NVAL))
+		return FALSE;
+
+	cli_sock = g_io_channel_unix_get_fd(io);
 
 	addrlen = sizeof(addr4);
 	memset(&addr4, 0, sizeof(addr4));
-	len = recvfrom(sock, buffer, sizeof(buffer), 0,
+	len = recvfrom(cli_sock, buffer, sizeof(buffer), 0,
 		       (struct sockaddr *) &addr4, &addrlen);
 
 	inet_ntop(AF_INET, &(addr4.sin_addr), ipv4_str, INET_ADDRSTRLEN);
-	hal_log_info("IPv4 recvfrom: %s(%zu)", ipv4_str, len);
+
+	watch = g_hash_table_lookup(inet_hash, ipv4_str);
+	if (watch)
+		sock = watch->sock;
+	else {
+		/* New peer */
+		sock = unix_connect();
+		if (sock < 0)
+			return TRUE;
+
+		watch = g_new0(struct watch, 1);
+		io = g_io_channel_unix_new(sock);
+		g_io_channel_set_close_on_unref(io, TRUE);
+		watch->sock = sock;
+		watch->id = g_io_add_watch_full(io, G_PRIORITY_DEFAULT,
+						 down_cond,
+						 downlink_cb,
+						 g_strdup(ipv4_str),
+						 downlink_destroy);
+		g_io_channel_unref(io);
+
+		g_hash_table_insert(inet_hash,
+				    g_strdup(ipv4_str),
+				    watch);
+	}
+
+	hal_log_info("%s, sock:%d, len:%zu", ipv4_str, sock, len);
+
+	len = write(sock, buffer, len);
+	if (len < 0) {
+		err = errno;
+		hal_log_info("write(): %s(%d)", strerror(err), err);
+	}
 
 	return TRUE;
 }
@@ -78,28 +181,65 @@ static gboolean read_inet4_cb(GIOChannel *io, GIOCondition cond,
 static gboolean read_inet6_cb(GIOChannel *io, GIOCondition cond,
 							gpointer user_data)
 {
+	GIOCondition down_cond = G_IO_ERR | G_IO_HUP | G_IO_NVAL | G_IO_IN;
 	struct sockaddr_in6 addr6;
+	struct watch *watch;
 	char buffer[1280];
 	char ipv6_str[INET6_ADDRSTRLEN];
 	socklen_t addrlen;
 	ssize_t len;
+	int err;
+	int cli_sock;
 	int sock;
 
 	if (cond & (G_IO_ERR | G_IO_HUP | G_IO_NVAL))
 		return FALSE;
 
-	sock = g_io_channel_unix_get_fd(io);
+	cli_sock = g_io_channel_unix_get_fd(io);
 
 	addrlen = sizeof(addr6);
 	memset(&addr6, 0, sizeof(addr6));
-	len = recvfrom(sock, buffer, sizeof(buffer), 0,
+	len = recvfrom(cli_sock, buffer, sizeof(buffer), 0,
 		       (struct sockaddr *) &addr6, &addrlen);
 
 	inet_ntop(AF_INET6, &(addr6.sin6_addr), ipv6_str, INET6_ADDRSTRLEN);
-	hal_log_info("IPv6 recvfrom: %s (%zu)", ipv6_str, len);
+
+	watch = g_hash_table_lookup(inet_hash, ipv6_str);
+	if (watch)
+		sock = watch->sock;
+	else {
+		/* New peer */
+		sock = unix_connect();
+		if (sock < 0)
+			return TRUE;
+
+		watch = g_new0(struct watch, 1);
+		io = g_io_channel_unix_new(sock);
+		g_io_channel_set_close_on_unref(io, TRUE);
+		watch->sock = sock;
+		watch->id = g_io_add_watch_full(io, G_PRIORITY_DEFAULT,
+						 down_cond,
+						 downlink_cb,
+						 g_strdup(ipv6_str),
+						 downlink_destroy);
+		g_io_channel_unref(io);
+
+		g_hash_table_insert(inet_hash,
+				    g_strdup(ipv6_str),
+				    watch);
+	}
+
+	hal_log_info("%s, sock:%d, len:%zu", ipv6_str, sock, len);
+
+	len = write(sock, buffer, len);
+	if (len < 0) {
+		err = errno;
+		hal_log_info("write(): %s(%d)", strerror(err), err);
+	}
 
 	return TRUE;
 }
+
 static int inet4_start(int port4)
 {
 	GIOCondition cond = G_IO_ERR | G_IO_HUP | G_IO_NVAL | G_IO_IN;
@@ -220,11 +360,25 @@ int manager_start(int port4, int port6)
 	if (ret < 0)
 		inet4_stop();
 
+	inet_hash = g_hash_table_new_full(g_str_hash, g_str_equal,
+						g_free, g_free);
 	return ret;
+}
+
+static void remove_downlink_source(gpointer key, gpointer value,
+						gpointer user_data)
+{
+	struct watch *watch = value;
+
+	g_source_remove(watch->id);
 }
 
 void manager_stop(void)
 {
 	inet4_stop();
 	inet6_stop();
+
+	g_hash_table_foreach(inet_hash, remove_downlink_source, NULL);
+
+	g_hash_table_destroy(inet_hash);
 }
