6e329f4a2eef68f070845f1212f0bf66945699d1
João Neto <joao.alexandre@cesar.org.br>
2018-06-26 16:51:19 -0300

    Displays connection failure properly

--- src/proto-ws.c
+++ src/proto-ws.c
@@ -596,6 +596,7 @@ static int callback_lws_ws(struct lws *wsi,
 		parse(session, in, len);
 		break;
 	case LWS_CALLBACK_CLIENT_CONNECTION_ERROR:
+		hal_log_info("WSI: %p Host may be invalid or unreachable", wsi);
 		break;
 	case LWS_CALLBACK_WSI_CREATE: // always protocol[0]
 		hal_log_info("WSI: %p created", wsi);
@@ -720,6 +721,9 @@ static int ws_connect(void)
 	info.userdata = NULL;
 
 	ws = lws_client_connect_via_info(&info);
+
+	if (!ws)
+		return -1;
 
 	sock = lws_get_socket_fd(ws);
 	l_hashmap_insert(lws_list, L_INT_TO_PTR(sock), ws);
