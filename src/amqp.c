672aed14a75f1ddc55178c43d8a9961750fb6676
Ramon Ribeiro <rhpr@cesar.org.br>
2019-08-16 15:38:16 -0400

    Connect to rabbitmq with default info
    This patch connect to default rabbitmq url:
    amqp://guest:guest@localhost:5672/

--- src/amqp.c
+++ src/amqp.c
@@ -27,19 +27,128 @@
 #define  _GNU_SOURCE
 #endif
 
+#include <stdio.h>
 #include <stdbool.h>
+#include <sys/time.h>
+#include <errno.h>
+#include <ell/ell.h>
+#include <hal/linux_log.h>
+#include <amqp.h>
+#include <amqp_tcp_socket.h>
 
 #include "settings.h"
 #include "amqp.h"
 
+#define AMQP_CONNECTION_TIMEOUT_US 10000
+
+static amqp_connection_state_t conn;
+
+static const char *amqp_server_exception_string(amqp_rpc_reply_t reply)
+{
+	amqp_connection_close_t *m = reply.reply.decoded;
+	static char r[512];
+
+	switch (reply.reply.id) {
+	case AMQP_CONNECTION_CLOSE_METHOD:
+		snprintf(r, sizeof(r),
+			 "server connection error %uh, message: %.*s\n",
+			 m->reply_code, (int)m->reply_text.len,
+			 (char *)m->reply_text.bytes);
+		break;
+	case AMQP_CHANNEL_CLOSE_METHOD:
+		snprintf(r, sizeof(r),
+			 "server channel error %uh, message: %.*s\n",
+			 m->reply_code, (int)m->reply_text.len,
+			 (char *)m->reply_text.bytes);
+		break;
+	default:
+		snprintf(r, sizeof(r),
+			 "unknown server error, method id 0x%08X\n",
+			 reply.reply.id);
+		break;
+	}
+
+	return l_strdup(r);
+}
+
+static const char *amqp_rpc_reply_string(amqp_rpc_reply_t reply)
+{
+	switch (reply.reply_type) {
+	case AMQP_RESPONSE_NONE:
+		return "missing RPC reply type!";
+	case AMQP_RESPONSE_LIBRARY_EXCEPTION:
+		return amqp_error_string2(reply.library_error);
+	case AMQP_RESPONSE_SERVER_EXCEPTION:
+		return amqp_server_exception_string(reply);
+	case AMQP_RESPONSE_NORMAL:
+	default:
+		return "";
+	}
+}
+
 int amqp_start(struct settings *settings)
 {
-	// TODO: connect with rabbitmq-server
-	return 0;
+	amqp_socket_t *socket;
+	struct amqp_connection_info cinfo;
+	amqp_rpc_reply_t r;
+	struct timeval timeout = { .tv_usec=AMQP_CONNECTION_TIMEOUT_US };
+	int status;
+
+	hal_log_dbg("Trying to connect with rabbitmq");
+	amqp_default_connection_info(&cinfo);
+	conn = amqp_new_connection();
+	socket = amqp_tcp_socket_new(conn);
+	if (!socket) {
+		hal_log_error("error creating tcp socket\n");
+		return -1;
+	}
+
+	status = amqp_socket_open_noblock(socket, cinfo.host, cinfo.port,
+					  &timeout);
+	if (status) {
+		hal_log_error("error opening socket\n");
+		return -1;
+	}
+
+	r = amqp_login(conn, cinfo.vhost, AMQP_DEFAULT_MAX_CHANNELS,
+			AMQP_DEFAULT_FRAME_SIZE, AMQP_DEFAULT_HEARTBEAT,
+			AMQP_SASL_METHOD_PLAIN, cinfo.user, cinfo.password);
+	if (r.reply_type != AMQP_RESPONSE_NORMAL) {
+		hal_log_error("amqp_login(): %s", amqp_rpc_reply_string(r));
+		return -1;
+	}
+	hal_log_info("Connected to amqp://%s:%s@%s:%d/%s", cinfo.user,
+					cinfo.password, cinfo.host, cinfo.port, cinfo.vhost);
+	amqp_channel_open(conn, 1);
+	r = amqp_get_rpc_reply(conn);
+	if (r.reply_type == AMQP_RESPONSE_NORMAL)
+		return 0;
+
+	hal_log_error("amqp_channel_open(): %s",
+				amqp_rpc_reply_string(r));
+
+	return -1;
 }
 
 void amqp_stop(void)
 {
-	// TODO: close connections with rabbitmq-server
-	return;
+	amqp_rpc_reply_t r;
+	int err;
+	if (!conn)
+		return;
+
+	r = amqp_channel_close(conn, 1, AMQP_REPLY_SUCCESS);
+	if (r.reply_type != AMQP_RESPONSE_NORMAL)
+		hal_log_error("amqp_channel_close: %s",
+				amqp_rpc_reply_string(r));
+
+	r = amqp_connection_close(conn, AMQP_REPLY_SUCCESS);
+	if (r.reply_type != AMQP_RESPONSE_NORMAL)
+		hal_log_error("amqp_connection_close: %s",
+				amqp_rpc_reply_string(r));
+
+	err = amqp_destroy_connection(conn);
+	if (err < 0)
+		hal_log_error("amqp_destroy_connection: %s",
+				amqp_error_string2(err));
 }
