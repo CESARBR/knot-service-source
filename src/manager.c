11c6e2c03b54d566fd79bae28547e681d1b8393d
Ramon Ribeiro <rhpr@cesar.org.br>
2019-11-20 15:00:50 -0300

    Revert "Remove cloud DBus interface"
    This reverts commit 66504ab305d1a26cb4e22d939c885141ea6a3677. Also, this
    patch removes the properties host, port and UUID.

--- src/manager.c
+++ src/manager.c
@@ -40,12 +40,84 @@
 #include "settings.h"
 #include "msg.h"
 #include "dbus.h"
+#include "storage.h"
 #include "manager.h"
+
+static bool property_get_token(struct l_dbus *dbus,
+				  struct l_dbus_message *msg,
+				  struct l_dbus_message_builder *builder,
+				  void *user_data)
+{
+	const struct settings *settings = user_data;
+
+	l_dbus_message_builder_append_basic(builder, 's', settings->token);
+	hal_log_info("Get('Token' = %s)", settings->token);
+
+	return true;
+}
+
+static struct l_dbus_message *property_set_token(struct l_dbus *dbus,
+					struct l_dbus_message *msg,
+					struct l_dbus_message_iter *new_value,
+					l_dbus_property_complete_cb_t complete,
+					void *user_data)
+{
+	struct settings *settings = user_data;
+	const char *token;
+
+	if (!l_dbus_message_iter_get_variant(new_value, "s", &token))
+		return dbus_error_invalid_args(msg);
+
+	l_free(settings->token);
+	settings->token = l_strdup(token);
+	hal_log_info("Set('Token' = %s)", settings->token);
+
+	storage_write_key_string(settings->configfd,
+				 "Cloud", "Token", token);
+
+	l_dbus_property_changed(dbus,
+				l_dbus_message_get_path(msg),
+				SETTINGS_INTERFACE,
+				"Token");
+
+	return l_dbus_message_new_method_return(msg);
+}
+
+static void setup_interface(struct l_dbus_interface *interface)
+{
+	if (!l_dbus_interface_property(interface, "Token", 0, "s",
+				       property_get_token,
+				       property_set_token))
+		hal_log_error("Can't add 'Token' property");
+}
 
 static void setup_complete(void *user_data)
 {
 	struct settings *settings = user_data;
+	const char *path = "/";
 	int err;
+
+	/* Manager object */
+	if (!l_dbus_register_interface(dbus_get_bus(),
+				       SETTINGS_INTERFACE,
+				       setup_interface,
+				       NULL, false))
+		hal_log_error("dbus: unable to register %s",
+			      SETTINGS_INTERFACE);
+
+	if (!l_dbus_object_add_interface(dbus_get_bus(),
+					 path,
+					 SETTINGS_INTERFACE,
+					 settings))
+		hal_log_error("dbus: unable to add %s to %s",
+			      SETTINGS_INTERFACE, path);
+
+	if (!l_dbus_object_add_interface(dbus_get_bus(),
+					 path,
+					 L_DBUS_INTERFACE_PROPERTIES,
+					 settings))
+		hal_log_error("dbus: unable to add %s to %s",
+			      L_DBUS_INTERFACE_PROPERTIES, path);
 
 	err = msg_start(settings);
 	if (err < 0)
